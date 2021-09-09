358d14ab40e21d4636e2cb2a2c3cde03d474cf0f
Yevhenii_dev <77569229+Yevhenii-Web-dev@users.noreply.github.com>
2021-09-09 12:23:54 +0200

    add4

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
+//slider settings================================================================================
+
 $(document).ready(function () {
    $('.slider__body').slick({
       arrows: false,
       dots: true,
       adaptiveHeight: true,
-      variableWidth: true,
       speed: 1000,
       infinite: false,
       initialSlide: 2,
+      responsive: [
+         {
+            breakpoint: 1001,
+            settings: {
+               dots: false,
+            }
+         }
+      ]
    });
    $('.slider_body').slick('setPosition');
-
 });
+//slider settings================================================================================
+
+
+
+
+//burger================================================================================
+$('.icon-menu').click(function (event) {
+   $(this).toggleClass('active');
+   $('.menu__body').toggleClass('active');
+   $('body').toggleClass('lock');
+});
+//burger================================================================================
+
+
+
+
+
+//scroll================================================================================
+$(document).ready(function () {
+   $("#menu").on("click", "a", function (event) {
+      event.preventDefault();
+      var id = $(this).attr('href'),
+         top = $(id).offset().top;
+      $('body,html').animate({ scrollTop: top }, 800);
+   });
+});
+//scroll================================================================================
+
